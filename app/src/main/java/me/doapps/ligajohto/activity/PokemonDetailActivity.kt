f8b43b7daf2b80c5f57139ecc7f52c40f504f896
Leonardo Cardenas <leocardenas2898@gmail.com>
2019-07-25 20:32:34 -0500

    Agregar detalle de pokemon

--- app/src/main/java/me/doapps/ligajohto/activity/PokemonDetailActivity.kt
+++ app/src/main/java/me/doapps/ligajohto/activity/PokemonDetailActivity.kt
@@ -2,12 +2,23 @@ package me.doapps.ligajohto.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_pokemon_detail.*
 import me.doapps.ligajohto.R
+import me.doapps.ligajohto.util.StatusBarUtil
 
 class PokemonDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_detail)
+        StatusBarUtil.statusBarChange(color = ContextCompat.getColor(this, R.color.red), window = window)
+        onClick()
+    }
+
+    private fun onClick() {
+        back_pokemon.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/me/doapps/ligajohto/adapter/PokemonAdapter.kt
+++ app/src/main/java/me/doapps/ligajohto/adapter/PokemonAdapter.kt
@@ -1,10 +1,13 @@
 package me.doapps.ligajohto.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_pokemon.view.*
 import me.doapps.ligajohto.R
+import me.doapps.ligajohto.activity.PokemonDetailActivity
 
 class PokemonAdapter : RecyclerView.Adapter<PokemonAdapter.PokemonHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonHolder {
@@ -21,7 +24,9 @@ class PokemonAdapter : RecyclerView.Adapter<PokemonAdapter.PokemonHolder>() {
 
     inner class PokemonHolder(v: View) : RecyclerView.ViewHolder(v) {
         init {
-
+            v.content_pokemon.setOnClickListener {
+                v.context.startActivity(Intent(v.context, PokemonDetailActivity::class.java))
+            }
         }
     }
 }
