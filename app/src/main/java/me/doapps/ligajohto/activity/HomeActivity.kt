1da6c5a149b06c5ce33290e3a7692a02097efb4c
Leonardo Cárdenas <leocardenas2898@gmail.com>
2019-08-01 19:10:29 -0500

    Agregar busqueda de items en el home

--- app/src/main/java/me/doapps/ligajohto/activity/HomeActivity.kt
+++ app/src/main/java/me/doapps/ligajohto/activity/HomeActivity.kt
@@ -2,6 +2,8 @@ package me.doapps.ligajohto.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_home.*
 import me.doapps.ligajohto.R
@@ -13,14 +15,26 @@ class HomeActivity : AppCompatActivity() {
 
     lateinit var homeAdapter: HomeAdapter
     lateinit var listHome: MutableList<Pair<String, String>>
+    private var listHomeFilter = mutableListOf<Pair<String, String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         StatusBarUtil.statusBarChange(window)
         listHome = Setting.listHome()
-        homeAdapter = HomeAdapter(listHome)
+        listHomeFilter = listHome
+        homeAdapter = HomeAdapter(listHome = listHome, listHomeFilter = listHomeFilter)
         recycler_home.adapter = homeAdapter
         recycler_home.layoutManager = GridLayoutManager(this, 2)
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                homeAdapter.filter.filter(s)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
     }
 }
--- app/src/main/java/me/doapps/ligajohto/adapter/HomeAdapter.kt
+++ app/src/main/java/me/doapps/ligajohto/adapter/HomeAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -12,18 +14,51 @@ import me.doapps.ligajohto.R
 import me.doapps.ligajohto.activity.PokedexActivity
 import me.doapps.ligajohto.config.Setting
 
-class HomeAdapter(var listHome: MutableList<Pair<String, String>>) : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
+class HomeAdapter(
+    var listHome: MutableList<Pair<String, String>>,
+    var listHomeFilter: MutableList<Pair<String, String>>
+) : RecyclerView.Adapter<HomeAdapter.HomeHolder>(),
+    Filterable {
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                if (charString.isEmpty()) {
+                    listHomeFilter = listHome
+                } else {
+                    val filteredList = mutableListOf<Pair<String, String>>()
+                    listHome.forEach {
+                        if (it.first.toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(it)
+                        }
+                    }
+                    listHomeFilter = filteredList
+                }
+
+                val filterResults = FilterResults()
+                filterResults.values = listHomeFilter
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                listHomeFilter = filterResults.values as MutableList<Pair<String, String>>
+                notifyDataSetChanged()
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeHolder {
         return HomeHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return listHome.size
+        return listHomeFilter.size
     }
 
     override fun onBindViewHolder(holder: HomeHolder, position: Int) {
-        holder.itemView.title_item.text = listHome[position].first
-        when (listHome[position].second) {
+        holder.itemView.title_item.text = listHomeFilter[position].first
+        when (listHomeFilter[position].second) {
             Setting.listHome()[0].second -> {
                 holder.itemView.item_content.setCardBackgroundColor(
                     ContextCompat.getColor(
@@ -80,7 +115,7 @@ class HomeAdapter(var listHome: MutableList<Pair<String, String>>) : RecyclerVie
     inner class HomeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         init {
             itemView.item_content.setOnClickListener {
-                if (listHome[0].first == listHome[adapterPosition].first) {
+                if (listHome[0].first == listHomeFilter[adapterPosition].first) {
                     itemView.context.startActivity(Intent(itemView.context, PokedexActivity::class.java))
                 } else {
                     Toast.makeText(itemView.context, "No se encuentra actividad con ese nombre", Toast.LENGTH_SHORT)
