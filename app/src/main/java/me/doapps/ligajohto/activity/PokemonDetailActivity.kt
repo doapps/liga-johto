be354754c1dc7eee7169c9b23fbb000d15db8b7b
Leonardo Cárdenas <leocardenas2898@gmail.com>
2019-08-06 16:10:31 -0500

    Agregar primera seccion del detalle Pokemon

--- app/src/main/java/me/doapps/ligajohto/activity/PokemonDetailActivity.kt
+++ app/src/main/java/me/doapps/ligajohto/activity/PokemonDetailActivity.kt
@@ -3,22 +3,30 @@ package me.doapps.ligajohto.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentPagerAdapter
 import kotlinx.android.synthetic.main.activity_pokemon_detail.*
 import me.doapps.ligajohto.R
+import me.doapps.ligajohto.adapter.DetailPagerAdapter
 import me.doapps.ligajohto.util.StatusBarUtil
 
 class PokemonDetailActivity : AppCompatActivity() {
 
+    private lateinit var detailPagerAdapter: FragmentPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_detail)
+        detailPagerAdapter = DetailPagerAdapter(supportFragmentManager)
         StatusBarUtil.statusBarChange(color = ContextCompat.getColor(this, R.color.red), window = window)
         onClick()
+        viewPagerPokemon.adapter = detailPagerAdapter
+        tabPager.setupWithViewPager(viewPagerPokemon)
     }
 
     private fun onClick() {
         back_pokemon.setOnClickListener {
             onBackPressed()
         }
+
     }
 }
--- app/src/main/java/me/doapps/ligajohto/adapter/DetailPagerAdapter.kt
+++ app/src/main/java/me/doapps/ligajohto/adapter/DetailPagerAdapter.kt
@@ -1,4 +1,34 @@
 package me.doapps.ligajohto.adapter
 
-class DetailPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import me.doapps.ligajohto.fragment.AboutFragment
+import me.doapps.ligajohto.fragment.EvolutionFragment
+import me.doapps.ligajohto.fragment.StatsFragment
+
+class DetailPagerAdapter(supportFragmentManager: FragmentManager) : FragmentPagerAdapter(supportFragmentManager) {
+    override fun getItem(position: Int): Fragment {
+        var fragment = Fragment()
+        when (position) {
+            0 -> fragment = AboutFragment()
+            1 -> fragment = EvolutionFragment()
+            2 -> fragment = StatsFragment()
+        }
+        return fragment
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        var title = ""
+        when (position) {
+            0 -> title = "About"
+            1 -> title = "Base Stats"
+            2 -> title = "Evolution"
+        }
+        return title
+    }
 }
